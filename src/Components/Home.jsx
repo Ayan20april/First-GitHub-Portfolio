2d76091c6b13f60ea7ef72cc13d177957903f783
Ayan Mahanta <112053665+Ayan20april@users.noreply.github.com>
2023-11-27 10:45:12 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/man-with-laptop.jpg";
+import image from "../images/man-with-laptop.jpeg";
 
 const imageAltText = "Adult man in his hand laptop";
 
